b6b53ce4e9403d067f061cf6722bdb2654b1e723
Tanya Marcol <tanya@webdeveloper.mx>
2014-02-14 16:24:39 -0800

    Mi prueba

---
+++ Index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Sonata Power - Repository</title>
+	</head>
+
+	<body>
+		<h1>Welcome to Sonata Power</h1>
+		<p>Repositorio de prueba</p>
+		<p>-Tanya</p>
+	<body>	
+</html>
